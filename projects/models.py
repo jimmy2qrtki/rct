54e8a55934afe4fa2567386cd2bd808f81f1d316
jimmy2qrtki <128320195+jimmy2qrtki@users.noreply.github.com>
2024-11-22 16:31:04 +0300

    добавил ближайшее событие для проекта в списке проектов

--- projects/models.py
+++ projects/models.py
@@ -1,13 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Project(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Убрали null=True
     name = models.CharField(max_length=255)
     description = models.TextField()
-    # Дополнительные поля для хранения Excel документа
     excel_file = models.FileField(upload_to='excel/', null=True, blank=True)
 
-    def __str__(self):
-        return self.name
+    def get_next_event(self):
+        today = timezone.now().date()
+        next_event = self.events.filter(event_date__gte=today).order_by('event_date').first()
+        return next_event
 
 class Event(models.Model):
     EVENT_TYPES = (
--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Project, Event
 from .forms import ProjectForm, EventForm
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def manage_projects(request):
-    projects = Project.objects.all()
-    return render(request, 'projects/manage_projects.html', {'projects': projects})
+    projects = Project.objects.filter(user=request.user)
+    projects_with_events = [(project, project.get_next_event()) for project in projects]
+    return render(request, 'projects/manage_projects.html', {'projects_with_events': projects_with_events})
 
 def edit_project(request, project_id):
     project = get_object_or_404(Project, pk=project_id)
@@ -23,11 +26,15 @@ def edit_project(request, project_id):
         'events': events
     })
 
+@login_required
 def create_project(request):
     if request.method == 'POST':
         form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
+            project = form.save(commit=False)
+            project.user = request.user
             form.save()
+            return redirect('edit_project', project_id=project.id)
     else:
         form = ProjectForm()
     return render(request, 'projects/create_project.html', {'form': form})
